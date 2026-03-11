29e6951886745de7c42747c7b4bfbd4db83e85bb
jeancarloaul <jean.aucapina@ucuenca.edu.ec>
2026-03-11 17:05:29 -0500

    chore: update project changes

--- lib/screens/role_selection_screen.dart
+++ lib/screens/role_selection_screen.dart
@@ -82,7 +82,7 @@ class RoleSelectionScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
+                    );
             },
           ),
         ),
